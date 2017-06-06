ac6d7461ad6172c0bb3c19c6fc0cb2dda1891571
Harvin Llanes <harvin.llanes@gmail.com>
2017-06-05 21:03:22 -0600

    Initial Commit

---
+++ ios_baseline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_baseline
+//
+//  Created by Harvin Llanes on 5/6/17.
+//  Copyright © 2017 Harvin Llanes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
